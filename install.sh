829beb3be280402162ac14acf1ef3fbed74050ce
huangbo <huangbo@yy.com>
2018-04-17 18:28:37 +0800

    update
    添加soda代码库

--- install.sh
+++ install.sh
@@ -4,15 +4,18 @@ CURRENT_DIR=$(cd "$(dirname "$0")"; pwd)
 PLUGIN_DIR="${HOME}/Library/Application Support/Developer/Shared/Xcode/Plug-ins/"
 PLUGIN_CONFIG_DIR="${HOME}/.uncrustify"
 SNIPPET_DIR="${HOME}/Library/Developer/Xcode/UserData/"
-TEMPLATE_DIR="/Applications/Xcode.app/Contents/Developer/Platforms/iPhoneOS.platform/Developer/Library/Xcode/"
+#TEMPLATE_DIR="/Applications/Xcode.app/Contents/Developer/Platforms/iPhoneOS.platform/Developer/Library/Xcode/"
+TEMPLATE_DIR="/Applications/Xcode.app/Contents/Developer/Library/Xcode/Templates/File Templates/Source"
 
 # 安装代码格式化插件
-mkdir -p "${PLUGIN_DIR}"
-sudo cp -rf "${CURRENT_DIR}/Plug-ins/UncrustifyPlugin.xcplugin" "${PLUGIN_DIR}"
+#mkdir -p "${PLUGIN_DIR}"
+#sudo cp -rf "${CURRENT_DIR}/Plug-ins/UncrustifyPlugin.xcplugin" "${PLUGIN_DIR}"
+echo '不安装代码格式化插件'
 
 # 格式化插件配置文件移动
-mkdir -p "${PLUGIN_CONFIG_DIR}"
-sudo cp -rf "${CURRENT_DIR}/uncrustify.cfg" "${PLUGIN_CONFIG_DIR}"
+#mkdir -p "${PLUGIN_CONFIG_DIR}"
+#sudo cp -rf "${CURRENT_DIR}/uncrustify.cfg" "${PLUGIN_CONFIG_DIR}"
+echo '不安装格式化配置文件插件'
 
 # # 安装代码模板
 mkdir -p "${TEMPLATE_DIR}"
