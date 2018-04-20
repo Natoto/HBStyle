4efe0c54bbf2455a1b30e4e65c9743de0feb3929
huangbo <huangbo@yy.com>
2018-04-20 08:25:07 +0800

    update

--- install.sh
+++ install.sh
@@ -7,15 +7,16 @@ SNIPPET_DIR="${HOME}/Library/Developer/Xcode/UserData/"
 #TEMPLATE_DIR="/Applications/Xcode.app/Contents/Developer/Platforms/iPhoneOS.platform/Developer/Library/Xcode/"
 TEMPLATE_DIR="/Applications/Xcode.app/Contents/Developer/Library/Xcode/Templates/File Templates/Source"
 
+echo "安装路径： ${SNIPPET_DIR}"
 # 安装代码格式化插件
 #mkdir -p "${PLUGIN_DIR}"
 #sudo cp -rf "${CURRENT_DIR}/Plug-ins/UncrustifyPlugin.xcplugin" "${PLUGIN_DIR}"
-echo '不安装代码格式化插件'
+#echo '不安装代码格式化插件'
 
 # 格式化插件配置文件移动
 #mkdir -p "${PLUGIN_CONFIG_DIR}"
 #sudo cp -rf "${CURRENT_DIR}/uncrustify.cfg" "${PLUGIN_CONFIG_DIR}"
-echo '不安装格式化配置文件插件'
+#echo '不安装格式化配置文件插件'
 
 # # 安装代码模板
 mkdir -p "${TEMPLATE_DIR}"
@@ -29,4 +30,4 @@ sudo cp -rf "${CURRENT_DIR}/CodeSnippets" "$SNIPPET_DIR"
 cd "${PLUGIN_DIR}"
 rm -rf __MACOSX
 
-echo "\nPAPH Code Style successfuly installed! Please restart your Xcode."
+echo "PAPH Code Style successfuly installed! Please restart your Xcode."
